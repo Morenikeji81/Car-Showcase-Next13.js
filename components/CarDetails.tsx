e9197bd5dc722ee95a6ac4a7102cb8c2d020039a
Adewale Oluwatoyin <adewaleoluwatoyin81@gmail.com>
2023-09-13 21:54:04 +0100

    My First project using next13.js (Car Showcase)

---
+++ components/CarDetails.tsx
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+import Image from 'next/image';
+
+import {Dialog, Transition } from '@headlessui/react'
+
+import { CarProps } from '@types';
+import React from 'react';
+
+interface CarDetailsProps {
+    isOpen: boolean;
+    closeModal: () => void;
+    car: CarProps;
+}
+
+const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps) => {
+  return (
+    
+  )
+}
+
+export default CarDetails
